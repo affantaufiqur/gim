6cbc6611b42301265983f5eab5cab5637b97c1a0
affantaufiqur <affantaufiqur@gmail.com>
2020-03-27 14:36:39 +0700

    -

---
+++ Assets/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int counter = 15;
+    public Text timerText;
+
+    void Start()
+    {
+    	timerText.text = counter.ToString();
+    	StartCoroutine(hitungmundur());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    IEnumerator hitungmundur(){
+    	while(counter > 0){
+    	yield return new WaitForSeconds(1);
+    	counter -= 1;
+    	timerText.text = counter.ToString();
+		}
+
+		timerText.text = "GAME OVER!";
+    }
+}
