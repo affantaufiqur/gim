09364e0c096a8e27c4b37f85d15a20a7912d85d9
affantaufiqur <affantaufiqur@gmail.com>
2020-04-03 14:29:31 +0700

    -

--- Assets/BallControl.cs
+++ Assets/BallControl.cs
@@ -4,39 +4,46 @@ using UnityEngine;
 
 public class BallControl : MonoBehaviour
 {
-    private Rigidbody2D rb2d;
-    void GoBall(){
-    float rand = Random.Range(0, 2);
-    if(rand < 1){
-        rb2d.AddForce(new Vector2(20, -15));
-    } else {
-        rb2d.AddForce(new Vector2(-20, -15));
-    }
-}
+    public int speed = 30;
+    public Rigidbody2D movement;
+    public Animator animatr;
+ 
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
-    Invoke("GoBall", 2);
+    GetComponent<Rigidbody2D>().velocity= new Vector2(1,-1)*speed;
+    movement.velocity = new Vector2(-1,-1) * speed;
+    animatr.SetBool("isMove", true);
+       
     }
-void ResetBall(){
-    rb2d.velocity = Vector2.zero;
-    transform.position = Vector2.zero;
-}
-void RestartGame(){
-    ResetBall();
-    Invoke("GoBall", 1);
-}
-void OnCollisionEnter2D (Collision2D coll) {
-    if(coll.collider.CompareTag("Player")){
-        Vector2 vel;
-        vel.x = rb2d.velocity.x;
-        vel.y = (rb2d.velocity.y / 2) + (coll.collider.attachedRigidbody.velocity.y / 3);
-        rb2d.velocity = vel;
+    void OnCollisionEnter2D(Collision2D other){
+    if(other.collider.name=="PlayerControl" || other.collider.name=="PlayerControl"){
+        GetComponent<Transform>().position = new Vector2(0,0);
     }
 }
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if(movement.velocity.x > 0){
+        	movement.GetComponent<Transform>().localScale = new Vector3(1,1,1);
+        	}else{
+        		movement.GetComponent<Transform>().localScale = new Vector3(-1,1,1);
+        	}
     }
+    /*void OnCollisionEnter2D (Collision2D coll) {
+    if(other.collider.name=="Kanan" || other.collider.name=="Kiri"{
+        StartCoroutine(jeda());
+    }
+}
+
+IEnumerator jeda(){
+	movement.velocity = Vector2.zero;
+	animtr.SetBool("isMove", false);
+	movement.GetComponent<Transform>().position = Vector2.zero;
+	yield return new WaitForSeconds(1);
+	movement.velocity = new Vector2(-1,-1) * speed;
+	animtr.SetBool("isMove", true);
+
+}*/
+
 }
