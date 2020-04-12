0ee17a88335c292e2c68b541be1dcbcba564f075
affantaufiqur <affantaufiqur@gmail.com>
2020-04-12 12:31:41 +0700

    -

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
@@ -29,6 +30,7 @@ public class Timer : MonoBehaviour
     	timerText.text = counter.ToString();
 		}
 
-		timerText.text = "GAME OVER!";
+		//timerText.text = "GAME OVER!";
+        SceneManager.LoadScene("GameOver");
     }
 }
